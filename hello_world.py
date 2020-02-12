2d1096e13caf758f4647c9ff513789c38c259116
DeZ-Dew <dewberryde@gmail.com>
2020-02-11 21:45:23 -0500

    update

---
+++ hello_world.py
@@ -0,0 +1,15 @@
+print("Hello World")
+name = "DeZavier!"
+print("Hello", name)
+name = "DeZ!"
+print("Hello " + name)
+num = "10!"
+print("Hello", num)
+num = "10!"
+print("Hello " + num)
+food_one = "Pizza"
+food_two = "Wings"
+print("I love to eat", food_one, "and", food_two)
+food_one = "laksa"
+food_two = "rendang"
+print(f"I love to eat {food_one} and {food_two}." )
